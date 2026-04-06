3cefc0ee1e997f5dbf05921c16b7ff6108b17284
ark2606 <ankash2609@gmail.com>
2026-04-06 21:24:34 +0530

    Delete logicalOperatorWithoutBoolean.js

--- logicalOperatorWithoutBoolean.js
+++
@@ -1,4 +0,0 @@
-console.log((false && 'ankit'))
-console.log((false || 'ankit'))
-
-console.log((false || 'github' || 8 || 18) ) // Output: "ankit" because it is the first truthy value in the expression short circuiting
